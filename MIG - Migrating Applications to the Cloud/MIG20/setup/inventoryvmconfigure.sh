8b97a8776f87483f44d62172e6f33324981942ce
Olivier Léger <olileger@microsoft.com>
2019-09-16 11:04:10 +0200

    Adding Docker install to Inventory services VM

--- MIG - Migrating Applications to the Cloud/MIG20/setup/inventoryvmconfigure.sh
+++ MIG - Migrating Applications to the Cloud/MIG20/setup/inventoryvmconfigure.sh
@@ -1,3 +1,12 @@
+# Install Docker ?
+sudo apt-get update
+sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo docker version
+
+
 sudo usermod -aG docker azureuser
 sudo docker login -u REPLACE_CONTAINER_REGISTRY_USERNAME -p REPLACE_CONTAINER_REGISTRY_PASSWORD REPLACE_CONTAINER_REGISTRY_SERVER
 sudo docker rm -f ignite-service
